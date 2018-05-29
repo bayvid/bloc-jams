423f00290e5d819efa931d996901152ff42a000b
David Morgan <dmorgan.bay@gmail.com>
2018-05-28 22:31:42 -0600

    Checkpoint 18 jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,28 @@
 
-var pointsArray = document.getElementsByClassName("point");
+var animatePoints = function() {
 
-var revealPoint = function () {
-    for(var i = 0; i < pointsArray.length; i++) {
-        pointsArray[i].style.opacity = 1;
-        pointsArray[i].style.transform = "scaleX(1) translateY(0)";
-        pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
-        pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-};
+    var revealPoint = function() {
+        $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     };
 
- window.onload = function() {
-     if (window.innerHeight > 950) {
-              revealPoint();
+         $.each($('.point'), revealPoint);
+
+     };
+
+ $(window).load(function() {
+
+     if ($(window).height() > 950) {
+         animatePoints();
           }
 
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+          var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-
-     window.addEventListener('scroll', function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                      revealPoint();
-                  }     });
-}
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+                  }
+              });
+          });
